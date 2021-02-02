690a5832e6ad12317ae8b3e671a94e622313eb30
hezhan <hezhan@zhuniu.com>
2021-02-02 09:43:41 +0800

    多线程--使用枚举实现单例模式(修改后的)

--- src/main/java/com/zhan/juc/volatiletest/single/EnumSingle.java
+++ src/main/java/com/zhan/juc/volatiletest/single/EnumSingle.java
@@ -10,15 +10,25 @@ import java.lang.reflect.InvocationTargetException;
  */
 public enum  EnumSingle {
 
-    INSTANCE;
+    INSTANCE("张三", 3);
 
+    // 这里还可以定义几个类的属性;
+    private String name;
+    private int age;
+
+    EnumSingle(String name, int age) {
+    }
+
+    /**
+     * 可以定义一些方法
+     */
     public void anyMethod(){
 
     }
 
 
     public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        Constructor<EnumSingle> declaredConstructor = EnumSingle.class.getDeclaredConstructor(String.class, int.class);
+        Constructor<EnumSingle> declaredConstructor = EnumSingle.class.getDeclaredConstructor(String.class, int.class, String.class, int.class);
         declaredConstructor.setAccessible(true);
 
         EnumSingle instance1 = declaredConstructor.newInstance();
